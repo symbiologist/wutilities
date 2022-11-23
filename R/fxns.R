5c29d7a9f201782e5b92798302fbdd54c11917ba
symbiologist <symbiologist@gmail.com>
2022-11-22 21:25:53 -0800

    Add rapid odds table functions

--- R/fxns.R
+++ R/fxns.R
@@ -132,239 +132,93 @@ apply_filters <- function(input,
   input[index_to_retain, ]
 }
 
-### enrichment
-tabulate_frequency <- function(metadata_table,
-                               group1 = guide_target,
-                               group2 = seurat_clusters,
-                               rename = TRUE) {
-  frequency_table <- 
-    metadata_table %>% 
-    mutate_all(as.factor) %>% 
-    mutate_all(droplevels) %>% 
-    dplyr::select({{group1}}, 
-                  {{group2}}) 
+#' Title
+#'
+#' @param group1 
+#' @param group2 
+#' @param overlap 
+#' @param total 
+#' @param clean 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+odds_quick <- function(group1,
+                       group2,
+                       overlap,
+                       total,
+                       clean = TRUE) {
   
-  if(rename) {
-    colnames(frequency_table) <- c('target', 'cluster')
+  if(length(group1) + length(group2) + length(overlap) == 3) {
+    
+    a <- overlap
+    b <- group1 - overlap
+    c <- group2 - overlap
+    d <- total - group1 - c
+    
   }
   
-  frequency_table %>% table()
+  contingency <- matrix(c(a, b,
+                          c, d),
+                        nrow = 2)
   
+  fisher <- fisher.test(contingency)
+  
+  if(clean) {
+    tibble('odds' = round(fisher$estimate, 2),
+           'log2odds' = round(log2(odds), 2),
+           'pval' = fisher$p.value)
+    
+  } else {
+    
+    tibble('n_group1', 
+           'n_group2',
+           'n_overlap',
+           'n_total',
+           'odds' = round(as.numeric(fisher$estimate), 2),
+           'log2odds' = round(log2(odds), 2),
+           'pval' = fisher$p.value)
+  }
 }
 
-calculate_proportions <- function(frequency_table) {
+#' Title
+#'
+#' @param df 
+#' @param group1 
+#' @param group2 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+odds_table <- function(df,
+                       group1,
+                       group2) {
   
-  target_proportion <- prop.table(frequency_table, margin = 1) %>% 
-    as.data.frame() %>% 
-    as_tibble() %>% 
-    mutate(target_proportion = round(Freq, 3)) %>% 
-    dplyr::select(-Freq)
-  
-  cluster_proportion <- prop.table(frequency_table, margin = 2) %>% 
-    as.data.frame() %>% 
-    as_tibble() %>% 
-    mutate(cluster_proportion = round(Freq, 3)) %>% 
-    dplyr::select(-Freq)
-  
-  frequency_long <- frequency_table %>% 
-    as.data.frame() %>% 
+  df %>% 
+    select(all_of(c(group1, group2))) %>% 
+    set_names(c('group1', 'group2')) %>% 
+    add_count(name = 'n_total') %>% 
+    group_by(group1) %>% 
+    add_count(name = 'n_group1') %>% 
+    group_by(group2) %>% 
+    add_count(name = 'n_group2') %>% 
+    group_by_all() %>% 
+    tally(name = 'n_overlap') %>% 
+    rowwise() %>% 
+    mutate(
+      f_group1 = n_overlap/n_group1,
+      f_group2 = n_overlap/n_group2,
+      f_max = max(f_group1, f_group2),
+      f_score = f_group1 * f_group2,
+      odds_quick(group1 = n_group1,
+                 group2 = n_group2,
+                 overlap = n_overlap,
+                 total = n_total,
+                 clean = TRUE)) %>% 
+    ungroup() %>% 
+    mutate(across(starts_with('f_'), round, 3)) %>% 
     as_tibble()
-  
-  colnames(frequency_long) <- c('target', 'cluster', 'n_overlap')
-  
-  cluster_totals <- colSums(frequency_table) %>% 
-    as.data.frame() %>% 
-    rownames_to_column()
-  
-  colnames(cluster_totals) <- c('cluster', 'n_cluster')
-  
-  target_totals <- rowSums(frequency_table) %>% 
-    as.data.frame() %>% 
-    rownames_to_column()
-  colnames(target_totals) <- c('target', 'n_target')
-  
-  proportion_table <- inner_join(cluster_proportion,
-                                 target_proportion) %>% 
-    inner_join(frequency_long) %>% 
-    inner_join(target_totals) %>% 
-    inner_join(cluster_totals) %>% 
-    mutate(max_proportion = ifelse(cluster_proportion > target_proportion, cluster_proportion, target_proportion),
-           score = round(cluster_proportion * target_proportion, 3)) %>% 
-    arrange(-max_proportion)
-  
-  proportion_table
-  
 }
-
-proportion_histograms <- function(proportion_table) {
-  list(proportion_table %>% ggplot(aes(x = cluster_proportion)) + geom_histogram() + scale_x_log10(),
-       proportion_table %>% ggplot(aes(x = target_proportion)) + geom_histogram() + scale_x_log10(),
-       proportion_table %>% ggplot(aes(x = score)) + geom_histogram() + scale_x_log10()) %>% 
-    wrap_plots()
-}
-
-create_proportion_matrix <- function(frequency_table) {
-  target_proportion <- prop.table(frequency_table, margin = 1)
-  
-  cluster_proportion <- prop.table(frequency_table, margin = 2)
-  
-  list('cluster' = as.matrix(cluster_proportion),
-       'target' = as.matrix(target_proportion))
-}
-
-proportion_analysis <- function(metadata_table,
-                                group1 = guide_target,
-                                group2 = seurat_clusters,
-                                rename = TRUE,
-                                heatmap_names = FALSE) {
-  
-  condition <- metadata_table$condition %>% unique()
-  object <- metadata_table$object %>% unique()
-  
-  freq_table <- metadata_table %>% 
-    tabulate_frequency(group1 = {{group1}},
-                       group2 = {{group2}},
-                       rename = rename)
-  
-  prop_table <- freq_table %>% calculate_proportions()
-  prop_matrix <- freq_table %>% create_proportion_matrix()
-  prop_hist <- prop_table %>% proportion_histograms()
-  
-  cluster_heatmap <- pheatmap(t(prop_matrix$cluster), 
-                              scale = 'column', 
-                              clustering_distance_rows = 'correlation', 
-                              clustering_distance_cols = 'correlation',
-                              main = paste(condition, object, 'cluster'),
-                              show_colnames = heatmap_names)
-  
-  target_heatmap <- pheatmap(t(prop_matrix$target), 
-                             scale = 'column', 
-                             clustering_distance_rows = 'correlation', 
-                             clustering_distance_cols = 'correlation',
-                             main = paste(condition, object, 'target'),
-                             show_colnames = heatmap_names)
-  
-  heatmaps <- list('cluster' = cluster_heatmap,
-                   'target' = target_heatmap)
-  
-  list('frequency' = freq_table,
-       'proportions' = prop_table,
-       'matrix' = prop_matrix,
-       'histogram' = prop_hist,
-       'heatmap' = heatmaps)
-}
-
-cluster_enrichment <- function(metadata_table,
-                               group1 = guide_target,
-                               group2 = seurat_clusters,
-                               rename = TRUE,
-                               print = FALSE,
-                               method = 'Fisher') {
-  
-  frequency_table <- metadata_table %>% 
-    dplyr::select(barcode,
-                  {{group1}}, 
-                  {{group2}})
-  
-  if(rename) {
-    colnames(frequency_table) <- c('barcode', 'target', 'cluster')
-  }
-  
-  targets <- frequency_table$target %>% unique() %>% sort()
-  clusters <- frequency_table$cluster %>% as.character() %>% unique() %>% sort()
-  all_barcodes <- frequency_table$barcode
-  
-  enrich <- map(targets, function(i) {
-    cat(red(paste('Running', i, '\n')))
-    
-    map(clusters, function(j) {
-      
-      target_subset <- frequency_table %>% filter(target == i) %>% pull(barcode) 
-      cluster_subset <- frequency_table %>% filter(cluster == j) %>% pull(barcode)
-      overlap_subset <- intersect(target_subset, cluster_subset)
-      
-      # run enrichment test
-      enrichment_res <- enrichment_test(target_subset,
-                                        cluster_subset,
-                                        background = all_barcodes,
-                                        print = print)
-      
-      odds <- enrichment_res$estimate
-      pval <- enrichment_res$p.value
-      
-      output <- tibble('target' = i,
-                       'cluster' = j,
-                       'n_overlap' = length(overlap_subset),
-                       'n_target' = length(target_subset),
-                       'n_cluster' = length(cluster_subset),
-                       'f_target' = n_overlap/n_target,
-                       'f_cluster' = n_overlap/n_cluster,
-                       'odds' = enrichment_res$fisher$estimate,
-                       'log2odds' = log2(odds),
-                       'pval' = enrichment_res$fisher$p.value)
-      output
-    }) %>% bind_rows()
-  }) %>% bind_rows()
-  
-  enrich %>% 
-    arrange(pval)
-}
-
-normalized_cluster_enrichment <- function(metadata_table,
-                                          group1 = guide_target,
-                                          group2 = seurat_clusters,
-                                          rename = TRUE,
-                                          print = FALSE,
-                                          method = 'Fisher') {
-  
-  frequency_table <- metadata_table %>% 
-    dplyr::select(barcode,
-                  {{group1}}, 
-                  {{group2}})
-  
-  if(rename) {
-    colnames(frequency_table) <- c('barcode', 'target', 'cluster')
-  }
-  
-  targets <- frequency_table$target %>% unique() %>% sort() %>% setdiff('Non-Targeting')
-  clusters <- frequency_table$cluster %>% as.character() %>% unique() %>% sort()
-  
-  enrich <- map(targets, function(i) {
-    cat(red(paste('Running', i, '\n')))
-    
-    map(clusters, function(j) {
-      
-      frequency_subset <- frequency_table %>% filter(target %in% c(i, 'Non-Targeting'))
-      target_subset <- frequency_subset %>% filter(target == i) %>% pull(barcode) 
-      cluster_subset <- frequency_subset %>% filter(cluster == j) %>% pull(barcode)
-      overlap_subset <- intersect(target_subset, cluster_subset)
-      barcodes_subset <- frequency_subset$barcode
-      
-      # run enrichment test
-      enrichment_res <- enrichment_test(target_subset,
-                                        cluster_subset,
-                                        background = barcodes_subset,
-                                        print = print)
-      
-      odds <- enrichment_res$estimate
-      pval <- enrichment_res$p.value
-      
-      output <- tibble('target' = i,
-                       'cluster' = j,
-                       'n_overlap' = length(overlap_subset),
-                       'n_target' = length(target_subset),
-                       'n_cluster' = length(cluster_subset),
-                       'f_target' = n_overlap/n_target,
-                       'f_cluster' = n_overlap/n_cluster,
-                       'odds' = enrichment_res$fisher$estimate,
-                       'log2odds' = log2(odds),
-                       'pval' = enrichment_res$fisher$p.value)
-      output
-    }) %>% bind_rows()
-  }) %>% bind_rows()
-  
-  enrich %>% 
-    arrange(pval)
-}
-
 ### sankey
